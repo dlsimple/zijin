13a9f79c767567eac0982d99b54cfdafdb51a106
dlsimple <dlsimple@gmail.com>
2025-05-25 22:26:11 +0800

    Refactor code structure for improved readability and maintainability

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import math
 import plotext as plt
 
+
 def put_next_max_ac(d, x, y):
-    if x >= 2 and (x-1) >= y:
-        key = (x - 1) ** 2 + y **2
+    if x >= 2 and (x - 1) >= y:
+        key = (x - 1) ** 2 + y**2
         if key not in d:
-            d[key] = (x-1, y)
+            d[key] = (x - 1, y)
     if y >= 2:
-        key = x ** 2 + (y-1) **2
+        key = x**2 + (y - 1) ** 2
         if key not in d:
-            d[key] = (x, y-1)
+            d[key] = (x, y - 1)
 
 
 def pop_max_ac(d: dict):
@@ -19,6 +20,7 @@ def pop_max_ac(d: dict):
     put_next_max_ac(d, max_ac_x, max_ac_y)
 
     return max_ac2, (max_ac_x, max_ac_y)
+
 
 def judge_square_sum(c, n):
     a = 0
@@ -33,13 +35,14 @@ def judge_square_sum(c, n):
             a += 1
         else:
             b -= 1
-    
+
     return False
+
 
 def judge_valid_triangle(ab2, bc2, ac2, c_point, n):
     (cx, cy) = c_point
     tmp2 = ab2 + ac2 - bc2
-    for i in range(0, n+1):
+    for i in range(0, n + 1):
         tmp = tmp2 - 2 * i * cx
         if tmp % (2 * cy) != 0:
             continue
@@ -48,38 +51,39 @@ def judge_valid_triangle(ab2, bc2, ac2, c_point, n):
         if j > n or j < 0:
             continue
 
-        if (i * i + j * j) != ab2 and (i - cx) * (i-cx) + (j-cy) * (j-cy) != bc2:
+        if (i * i + j * j) != ab2 and (i - cx) * (i - cx) + (j - cy) * (j - cy) != bc2:
             continue
-        
+
         return (i, j)
 
     return False
 
+
 def max_similar_triangle(ab2, bc2, ac2, n):
     d = {}
-    d[n * n * 2] = (n,n)
-
+    d[n * n * 2] = (n, n)
 
     while d:
         max_ac2, (cx, cy) = pop_max_ac(d)
-        if (max_ac2 * ab2) % ac2 != 0 or (max_ac2 * bc2 ) % ac2 != 0:
+        if (max_ac2 * ab2) % ac2 != 0 or (max_ac2 * bc2) % ac2 != 0:
             continue
 
         max_ab2 = max_ac2 * ab2 // ac2
         max_bc2 = max_ac2 * bc2 // ac2
 
-        if judge_square_sum(max_ab2, n) is False or judge_square_sum(max_bc2, n) is False:
+        if (
+            judge_square_sum(max_ab2, n) is False
+            or judge_square_sum(max_bc2, n) is False
+        ):
             continue
 
-
-        b_point = judge_valid_triangle(max_ab2, max_bc2, max_ac2, (cx,cy), n)
+        b_point = judge_valid_triangle(max_ab2, max_bc2, max_ac2, (cx, cy), n)
         if b_point is False:
             continue
 
         return b_point, (cx, cy)
-            
-    return False, False
 
+    return False, False
 
 
 def draw_triangle(b_point, c_point, n):
@@ -87,48 +91,50 @@ def draw_triangle(b_point, c_point, n):
     plt.plot_size(width=80, height=30)
     plt.xlim(0, n)
     plt.ylim(0, n)
-    plt.xticks(range(0, n+1))
-    plt.yticks(range(0, n+1))
+    plt.xticks(range(0, n + 1))
+    plt.yticks(range(0, n + 1))
 
     plt.xlabel("X")
     plt.ylabel("Y")
 
-    (bx,by) = b_point
-    (cx,cy) = c_point
+    (bx, by) = b_point
+    (cx, cy) = c_point
 
-    draw_x = [0, bx, cx]  
+    draw_x = [0, bx, cx]
     draw_y = [0, by, cy]
-    
-    draw_x = draw_x + [draw_x[0]]  
+
+    draw_x = draw_x + [draw_x[0]]
     draw_y = draw_y + [draw_y[0]]
 
     plt.plot(draw_x, draw_y)
     plt.show()
 
 
-
 def main():
     try:
         while True:
             try:
-                ab2, bc2, ac2, n = map(int, input("请输入三角形三边的平方数和正方形的格数（空格隔开）：").split())
-                
+                ab2, bc2, ac2, n = map(
+                    int,
+                    input(
+                        "请输入三角形三边的平方数和正方形的格数（空格隔开）："
+                    ).split(),
+                )
+
                 (b_poinx, c_point) = max_similar_triangle(ab2, bc2, ac2, n)
 
                 if b_poinx is False or c_point is False:
-                    print("没有找到符合条件的三角形")
+                    print("没有找到符合条件的三角形", end=" ")
                 else:
                     draw_triangle(b_poinx, c_point, n)
-                    print(f"a[0,0], b[{b_poinx}], c[{c_point}]")
+                    print(f"b{b_poinx}, c{c_point}", end=" ")
 
-                print("\n或按 Ctrl+C 退出\n")
+                print("或按 Ctrl+C 退出\n")
             except ValueError:
-                print("输入错误，请重新输入")
+                print("输入格式错误\n")
     except KeyboardInterrupt:
-        print("\n检测到 Ctrl+C，程序即将退出")
+        pass
+
 
 if __name__ == "__main__":
     main()
-
-
-
